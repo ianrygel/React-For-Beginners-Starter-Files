8dfb789febe89ffb019e391b986bac5b2353a1db
Ian Pomanitskiy <ipo@scalepoint.com>
2020-01-24 18:07:50 +0200

    First commit - 12 lessons

---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Order extends React.Component {
+
+    render() {
+        return(
+            <div className="order">
+                Orders!!!
+            </div>
+        )
+    }
+}
+
+export default Order
